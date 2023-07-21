4c11e7f693c50408bf893c5a4fbd3c982304515a
jwebwatchdog <138643354+jwebwatchdog@users.noreply.github.com>
2023-07-21 15:06:43 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,16 @@
 if __name__== "__main__":
  print("Hello World from the Devops repo!")
+
+import os
+ 
+hostname = "google.com"
+ 
+response = os.system("ping -n 1 " + hostname)
+ 
+if response == 0:
+ 
+    Netstatus = "Network Active"
+    
+else:
+ 
+    Netstatus = "Network Error"
